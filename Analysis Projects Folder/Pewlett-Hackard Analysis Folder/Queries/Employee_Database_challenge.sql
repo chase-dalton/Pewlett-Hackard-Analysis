0299862f696759f8bc0163d655c9739d4a374c06
Chase Dalton <chase.ryan.dalton@gmail.com>
2021-08-01 12:33:39 -0500

    Deliverable 2

--- Analysis Projects Folder/Pewlett-Hackard Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -25,26 +25,25 @@ order by emp_no asc, to_date desc;
 select
 count(ut.emp_no) as employee_count,
 ut.title
-INTO retiring_titles
+into retiring_titles
 from unique_titles as ut
 group by title 
 order by employee_count desc;
 
 
-SELECT DISTINCT ON(e.emp_no)e.emp_no,
+select distinct on (e.emp_no)e.emp_no,
 e.first_name,
 e.last_name,
 e.birth_date,
 de.from_date,
 de.to_date,
 t.title
---DISTINCT ON(emp_no)
---INTO mentor_eligibility
-FROM employees AS e
-INNER JOIN dept_emp AS de
-ON (e.emp_no = de.emp_no)
-INNER JOIN titles AS t
-ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
-AND (de.to_date = '9999-01-01')
-ORDER BY emp_no;
+into mentor_eligibility
+from employees as e
+inner join dept_emp as de
+on (e.emp_no = de.emp_no)
+inner join titles as t
+on (e.emp_no = t.emp_no)
+where (e.birth_date between '1965-01-01' and '1965-12-31')
+and (de.to_date = '9999-01-01')
+order by emp_no asc;
